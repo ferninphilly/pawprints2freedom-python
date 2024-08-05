75218037438d4bc18325daffea9f4a8225302105
kerrynakayama <kerry.nakayama@gmail.com>
2024-08-05 12:36:02 -0700

    Update __init__.py
    upates pip install and import libraries needed

--- GoogleSheets/__init__.py
+++ GoogleSheets/__init__.py
@@ -1,1 +1,7 @@
-from .GoogleSheets import GoogleSheets
+pip install gspread
+pip install gspread-dataframe
+
+import gspread
+from google.oauth2.service_account import Credentials
+from gspread_dataframe import set_with_dataframe
+import pandas as pd
